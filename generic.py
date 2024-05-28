77d92f3b9db03500f3ed327f3b1b3d5741ae251f
Caio Oliveira <xcaio2@gmail.com>
2024-05-28 18:17:53 +0100

    5 sites incomplete infos printing (no capacity or wrong text)

--- generic.py
+++ generic.py
@@ -10,7 +10,7 @@ options = Options()
 options.add_experimental_option("detach", True)
 # options.add_argument('--headless')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.maximize_window()
+# driver.maximize_window()
 
 # -------- SqlAlchemy config -------
 connection_string = "mysql+mysqlconnector://root:6F4LDiTvazP7Tk@127.0.0.1:3306/mock"
@@ -27,18 +27,27 @@ stores = list(stores.mappings())
 # for e in stores:
 #     print(e)
 
-# ----------- testing xpaths ---------
-# searching mateus+rose(one product only) on 4 stores
-for e in stores:
-    urls_lst = []
-    driver.get(e['search_url'] + 'mateus+rose')
-    x_cookie = e['x_cookies']
-    time.sleep(2)
+# ------------- searching mateus+rose(one product only) on 5 stores -------------
+for store in stores:
+    driver.get(store['search_url'] + 'mateus+rose')
+    x_cookie = store['x_cookies']
+    time.sleep(2)  # waits till the document loads entirely
     accept_cookies = driver.find_element("xpath", f"{x_cookie}")
     accept_cookies.click()
     # time.sleep(2)
-    x_pdp_url = e['x_url']
-    urls = driver.find_elements("xpath", f"{x_pdp_url}")
-    for url in urls:
-        urls_lst.append(url.text)
-    print(urls_lst)
+    x_pdp_url = store['x_url']
+    elements = driver.find_elements("xpath", f"{x_pdp_url}")
+    # python list comprehension for list urls_lst: https://www.w3schools.com/python/python_lists_comprehension.asp
+    urls_lst = [element.get_attribute("href") for element in elements]
+
+    for url in urls_lst:
+        driver.get(url)
+        # time.sleep(2)
+        print("Going to:", url)
+        name = driver.find_element("xpath", store['x_name'])
+        print("Name:", name.text)
+        capacity = driver.find_element("xpath", store['x_capacity'])
+        print("Capacity:", capacity.text)
+        price = driver.find_element("xpath", store['x_price'])
+        print("Price:", price.text)
+        print("-------------------------------------------------------------------------------------------------------")

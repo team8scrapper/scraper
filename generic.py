16553cce44feb5509c4b687c436500fd3b39d39d
Caio Oliveira <xcaio2@gmail.com>
2024-06-01 14:47:32 +0100

    all funcs but the matching one

--- generic.py
+++ generic.py
@@ -1,17 +1,20 @@
+from defusedxml import lxml
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 from sqlalchemy import create_engine, text
 from lxml import etree
+import types
 
 # -------- SELENIUM config ---------
 options = Options()
 options.add_experimental_option("detach", True)
-options.add_argument('--headless')
+options.add_argument('--headless')  # Uncomment this section to run the app HEADLESS
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-# driver.maximize_window()
+driver.maximize_window()
 
 # -------- SqlAlchemy config -------
 connection_string = "mysql+mysqlconnector://root:6F4LDiTvazP7Tk@127.0.0.1:3306/mock"
@@ -19,72 +22,104 @@ engine = create_engine(connection_string, echo=True)
 
 # ------- addressing STORE table's rows(db) to the variable store--------
 with engine.connect() as connection:
+    products = connection.execute(text("SELECT * FROM products"))
     stores = connection.execute(text("SELECT * FROM stores"))
-    # products = connection.execute(text("SELECT * FROM products"))
 
-stores = list(stores.mappings())
-products = {'name': 'mateus+rose'}  # TODO: change to the line underneath
+products = list(products.mappings())
 
 
-# products = list(products.mappings())
+# stores = list(stores.mappings())
+
+
+# products = {'name': 'mateus+rose'}  # TODO: change to the line underneath
 
 
 def take_screenshot(url: str, xpath: str, file_name: str) -> None:
     driver.get(url)
-    time.sleep(2)  # waits till the content load
     driver.maximize_window()
     body = driver.find_element("xpath", xpath)
+    wait = WebDriverWait(driver, timeout=4)
+    wait.until(lambda d: body.is_displayed())
     body.screenshot(f'{file_name}')
 
 
-def handle_start_page(search_url: str, product_name: str, x_cookie: str) -> None:
-    driver.get(search_url + product_name)
-    time.sleep(2)  # waits till the document loads entirely TODO "method to wait content to load instead of sleep"
-    # take_screenshot(search_url + product_name, '//body', 'test.png')
+def gather_pdp_urls(store) -> list:
+    global urls_lst
+    x_pdp_url = store['x_url']
+    urls_element = driver.find_elements("xpath", f"{x_pdp_url}")
+    if urls_element:
+        urls_lst = [element.get_attribute("href") for element in urls_element]
+
+    return urls_lst
+
+
+def handle_start_page(stores) -> None:
+    # stores_name = stores['name']
+    # take_screenshot(search_url + product_name, '//body', f'screenshots/{stores_name}.png')
+    cookies = stores['x_cookies']
     try:
-        driver.find_element("xpath", f"{x_cookie}")
-        accept_cookies = driver.find_element("xpath", f"{x_cookie}")
+        driver.find_element("xpath", f"{cookies}")
+        accept_cookies = driver.find_element("xpath", f"{cookies}")
+        wait = WebDriverWait(driver, timeout=4)
+        wait.until(lambda d: accept_cookies.is_displayed())
         accept_cookies.click()
     except:
         pass
 
 
-# ------------- searching mateus+rose(one product only) on 5 stores -------------
-urls_lst = []
-for store in stores:
-    # uncomment to test each store separately according to "enable" column in DB
-    if store['enabled'] == 0:
-        continue
-    for product in products:
-        # iterates on the products pages(url)
-        handle_start_page(store['search_url'], products['name'], store['x_cookies'])
-        x_pdp_url = store['x_url']
-        elements = driver.find_elements("xpath", f"{x_pdp_url}")
-        urls_lst = [element.get_attribute("href") for element in elements]
-
-    for url in urls_lst:
+def objxs_pdp_builder(urls_list) -> list:
+    objxs_list = []
+    for url in urls_list:
         driver.get(url)
-        # driver.add_cookie({"age_gate": '18'})
-        time.sleep(2)
+
         soup = driver.page_source
         dom = etree.HTML(str(soup))
+        # print("-->Going to:", store['name'], url)
 
-        print("-->Going to:", store['name'], url)
-        name = dom.xpath(store['x_name'])[0]
         try:
-            print("Name:", name.text)
+            name = (dom.xpath(store['x_name'])[0]).text
+            # print("name: ", name)
         except AttributeError:
-            print("Name:", name)
-        capacity = dom.xpath(store['x_capacity'])[0]
+            name = dom.xpath(store['x_name'])[0]
+        except IndexError:
+            name = (dom.xpath(store['x_name']))
         try:
-            print("Capacity:", capacity.text)
+            capacity = (dom.xpath(store['x_capacity'])[0]).text
+            # print("capacity", capacity)
         except AttributeError:
-            print("Capacity:", capacity)
-        price = dom.xpath(store['x_price'])[0]
+            capacity = dom.xpath(store['x_capacity'])[0]
+        except IndexError:
+            capacity = dom.xpath(store['x_capacity'])
         try:
-            print("Price:", price.text)
+            price = (dom.xpath(store['x_price'])[0]).text  # if isinstance(price, etree._ElementUnicodeResult):
+            # print("price: ", price)
         except AttributeError:
-            print("Price:", price)
+            price = dom.xpath(store['x_price'])[0]
+        except IndexError:
+            price = dom.xpath(store['x_price'])
 
-        print("------------------------------------------------------------------------------------------"
-              "------------------------------------------------------------------------------------------")
+        objxs_list.append({'name': f'{name}', 'capacity': f'{capacity}', 'price': f'{price}'})
+    print(f"\n{len(objxs_list)} object(s) created: ", objxs_list)
+    print("---------------------------------------------------------------------------------------------------------")
+    return objxs_list
+
+
+# ------------- searching mateus+rose(one product only) on 5 stores -------------
+urls_lst = []
+for store in stores.mappings():
+    # ---- uncomment this section to test each store separately according to the "enable" column in DB ----
+    # if store['enabled'] == 0:
+    #     continue
+    upper_name = (store['name']).upper()
+    print(f"\nGoing for: {upper_name}")
+    for product in products:
+        # name = "+".join(product['name'].split())
+        driver.get(store['search_url'] + product['name'])
+        handle_start_page(store)
+        urls_lst = gather_pdp_urls(store)
+        # print("urls mini_set:", urls_lst)
+        print(f"\nSearching for {product['name']}: ")
+        objxs_pdp_builder(urls_lst)
+        # pd_match(objxs_pdp_builder())
+
+print("---------------------------------...-->|Scraper is done|<--...----------------------------------------------")
